4640ab3ce47ad424fc928bd099edddfbb099137d
Nilesh <nbgosavi5@gmail.com>
2024-10-27 04:46:13 +0530

    added function to handle client side routing

--- src/app.js
+++ src/app.js
@@ -17,6 +17,20 @@ const app = express(); //initialising express
 connectdb();
 
 //middlewares
+// This code makes sure that any request that does not matches a static file
+// in the build folder, will just serve index.html. Client side routing is
+// going to make sure that the correct content will be loaded.
+app.use((req, res, next) => {
+  if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+      next();
+  } else {
+      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+      res.header('Expires', '-1');
+      res.header('Pragma', 'no-cache');
+      res.sendFile(path.join(path.resolve(), 'dist', 'index.html'));
+  }
+});
+
 // app.use(cors(corsOptions));//allowing cross origin access
 app.use(cookieParser());//middleware for parsing cookies
 app.use(morgan("dev"));// logs request to terminal
@@ -28,6 +42,7 @@ app.use("/api/", cartRouter) // routes related to cart
 app.use("/api/", userRouter) // routes related to users
 app.use("/api/", productsRouter)//routes related to products
 app.use("/api/", orderRouter); //routes related to orders
+
 
 app.get("/", (req, res)=>{
   res.sendFile(path.join(path.resolve(), './dist/index.html'));
